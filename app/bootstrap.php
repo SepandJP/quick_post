2e311b7d59c29c2971f3f0cca72198f949ec9bd3
Sepand JamshidPour <sepand100@gmail.com>
2021-04-01 07:53:40 +0430

    add autoloader for libraries file

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,9 +2,14 @@
 
 /*
  * require main files
- * Load Libraries
  * */
 
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/Database.php';
+// Load Config
+require_once 'config/config.php';
+
+// Autoload Core Libraries
+spl_autoload_register(function ($className)
+{
+    $libraryPath = 'libraries/' . $className . '.php';
+   require_once $libraryPath;
+});
