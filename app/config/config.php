376a979eeabce049dc5123817ce88145643134f7
Sepand JamshidPour <sepand100@gmail.com>
2021-04-01 09:38:10 +0430

    Define config

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// App Root
+$appRoot = dirname(__FILE__, 2);
+define('APPROOT', $appRoot);
+
+// URL Root
+$urlRoot = 'http://localhost/my_projects/quick_post/public';
+define('URLROOT', $urlRoot);
+
+// Site Root
+$siteName = 'QuickPost';
+define('SITENAME', $siteName);
