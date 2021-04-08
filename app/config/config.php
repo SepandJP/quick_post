cbbbb975c6b0b07b852f3fe5b0b563bd4c915273
Sepand JamshidPour <sepand100@gmail.com>
2021-04-08 19:43:25 +0430

    define constants

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * Define site paths
+ * Change these when upload or download project
+*/
 // App Root
 $appRoot = dirname(__FILE__, 2);
 define('APPROOT', $appRoot);
@@ -11,3 +14,17 @@ define('URLROOT', $urlRoot);
 // Site Root
 $siteName = 'QuickPost';
 define('SITENAME', $siteName);
+
+
+
+// DB Params
+/**
+ * These constants are for connect to DataBase
+ * Change these when upload or download project
+ */
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'qp_social_network');
+
+
