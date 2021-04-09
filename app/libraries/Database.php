221ae3a90c4fa9f081de4b04c8b98509bf039891
Sepand JamshidPour <sepand100@gmail.com>
2021-04-09 07:49:29 +0430

    use query in DB

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -17,8 +17,8 @@ class Database
      */
     private $dataBaseHandler;
     /**
-     * @var PDO
-     *
+     * @var PDOStatement
+     * For bind param
      */
     private $statement;
     /**
@@ -60,5 +60,90 @@ class Database
             $this->error = $e->getMessage();
             echo $this->error;
         }
+    }
+
+    /**
+     * Prepare statement with query
+     *
+     * @param string $sql MySQL query
+     */
+    public function query($sql)
+    {
+        $this->statement = $this->dataBaseHandler->prepare($sql);
+
+    }
+
+    /**
+     * For binding values of prepare statements
+     *
+     * @param string $param named parameters in prepared statements of MySql query
+     * @param int | bool | string $value values of named parameters
+     * @param int $type Predefined Constants of data types in PDO
+     */
+    public function bind($param, $value, $type = null)
+    {
+        // Define type
+        if (is_null($type))
+        {
+            switch (true)
+            {
+                case is_int($type):
+                    $type = PDO::PARAM_INT;
+                    break;
+
+                case is_bool($type):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+
+                case is_null($type):
+                    $type = PDO::PARAM_NULL;
+                    break;
+
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->statement->bindValue($param, $value, $type);
+    }
+
+    /**
+     * Execute the prepare statement
+     */
+    public function executeQuery()
+    {
+        $this->statement->execute();
+    }
+
+    /**
+     * Get result set as array of objects
+     *
+     * @return array return as an array of objects
+     */
+    public function getAllResult()
+    {
+        $this->executeQuery();
+        return $this->statement->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    /**
+     * Get single record as object
+     *
+     * @return mixed
+     */
+    public function getSingleResult()
+    {
+        $this->executeQuery();
+        return $this->statement->fetch(PDO::FETCH_OBJ);
+    }
+
+    /**
+     * Get row count of result
+     *
+     * @return int
+     */
+    public function rowCount()
+    {
+        return $this->statement->rowCount();
     }
 }
