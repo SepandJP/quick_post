be863cfc717eb87ee5ca71aa51a11478fe5b4ffb
Sepand JamshidPour <sepand100@gmail.com>
2021-04-19 19:15:29 +0430

    import bootstrap framework

--- app/views/include/header.php
+++ app/views/include/header.php
@@ -5,8 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <!--  import CSS files  -->
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <!--  Bootstrap icons  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
 
     <title><?php echo SITENAME;?></title>
 </head>
+
+
 <body>
+
+<?php require APPROOT . '/views/include/navbar.php'; ?>
+
+
+<div class="container">
