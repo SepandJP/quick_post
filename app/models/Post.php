2757d937ac8c0fa17a8e7445aa43706fdc83d7ab
Sepand JamshidPour <sepand100@gmail.com>
2021-07-19 13:02:39 +0430

    create Posts Model

--- app/models/Post.php
+++ app/models/Post.php
@@ -3,5 +3,26 @@
 
 class Post
 {
+    private $db;
 
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getAllPosts()
+    {
+        $query = 'SELECT *,
+                        posts.created_at as postCreated,
+                        users.name as userName
+                        FROM posts
+                        INNER JOIN users
+                        ON posts.user_id = users.id
+                        ORDER BY postCreated DESC';
+
+        $this->db->query($query);
+        $result = $this->db->getAllResult();
+
+        return $result;
+    }
 }
