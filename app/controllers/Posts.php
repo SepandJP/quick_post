8e89cef492e5ae9f8628a516d9407fdbd0ba408d
Sepand JamshidPour <sepand100@gmail.com>
2021-07-19 13:04:27 +0430

    create the Posts Controller

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,7 +1,42 @@
 <?php
 
-
-class posts
+class Posts extends Controller
 {
 
+    /**
+     * @var mixed
+     *           For connecting to the Posts Model
+     */
+    private $postModel;
+
+    /**
+     * Posts constructor.
+     *
+     * If the users don't log in site, they redirect to the login page and do not have access to see and modify the posts.
+     * // todo users without log in site, can see the posts, but do not have access to modify the posts.
+     */
+    public function __construct()
+    {
+        if (!isLoggedIn())
+        {
+            redirect('users/login');
+        }
+
+        $this->postModel = $this->loadModels('Post');
+    }
+
+    /**
+     *
+     */
+    public function index()
+    {
+        // Get all of the posts
+        $posts = $this->postModel->getAllPosts();
+
+        $data = [
+            'posts' => $posts
+        ];
+
+        $this->loadView('posts/index', $data);
+    }
 }
