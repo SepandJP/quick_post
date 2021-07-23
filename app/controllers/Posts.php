e9609ffcb61292dcf0d3db3f0616cfe57a424eae
Sepand JamshidPour <sepand100@gmail.com>
2021-07-23 12:42:36 +0430

    Write controller for edit posts

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -145,8 +145,72 @@ class Posts extends Controller
         }
     }
 
-    public function edit()
+    public function edit($id)
     {
+        // Check for existing the post
+        if (is_null($id) || !($this->postModel->getPostById($id)))
+        {
+            redirect('posts');
+        }
+        else {
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                // Sanitize POST array
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
+                $data = [
+                    'id' => $id,
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_error' => '',
+                    'body_error' => ''
+                ];
+
+
+                // Validate data
+                if (empty($data['title'])) {
+                    $data['title_error'] = 'Please enter title';
+                }
+                if (empty($data['body'])) {
+                    $data['body_error'] = 'Please enter body text';
+                }
+
+                // Make sure that no errors
+                if (empty($data['title_error']) && empty($data['body_error'])) {
+                    /*
+                     *  Validated
+                     * then insert post to database
+                     * and show successful message
+                    */
+                    if (!($this->postModel->addPost($data))) {
+                        flashMessages('updatePost', 'Post Updated Successfully');
+                        redirect('posts');
+                    } else {
+                        die('Oh, Something went wrong...');
+                    }
+                } else {
+                    // Load view with errors
+                    $this->loadView('posts/add', $data);
+                }
+            } else {
+                // Get existing post from model
+                $post = $this->postModel->getPostById($id);
+
+                // Check for owner
+                if ($post->user_id != $_SESSION['user_id'])
+                {
+                    redirect('posts');
+                }
+
+                $data = [
+                    'id' => $id,
+                    'title' => $post->title,
+                    'body' => $post->body
+                ];
+
+                // Load add post page with error
+                $this->loadView('posts/edit', $data);
+            }
+        }
     }
 }
